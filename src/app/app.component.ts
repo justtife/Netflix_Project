b2bcb526316e5dfc54dcc7422c41852158717643
Justtife <farinubolu@gmail.com>
2023-03-30 01:45:37 +0200

    Testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Netflix_Project';
+  constructor(private headerTitle: Title, private meta: Meta) {}
+  ngOnInit(): void {
+    this.headerTitle.setTitle('Product Page - This is the product page');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Hello World',
+    });
+  }
 }
